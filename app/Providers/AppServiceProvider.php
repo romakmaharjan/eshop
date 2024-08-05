41683342578d85b9c75915b1a4dd1b5caf214fa8
romak maharjan <romakmaharjan@gmail.com>
2024-08-05 15:35:01 +0545

    add code to make Login and Register system and link CSS and JS in project

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringlenght(191);
+        // Schema::defaultStringlenght(191);
     }
 }
